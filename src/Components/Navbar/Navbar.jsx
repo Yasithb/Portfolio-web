99631ab9ad75217f272868c244c5c9da40f61801
Yasithb <150070850+Yasithb@users.noreply.github.com>
2025-03-03 08:17:14 +0530

    Create Navbar.jsx

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.svg'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={logo} alt="" />
+      <ul className="nav-menu">
+        
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar

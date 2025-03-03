6873344f8f5a64cac62bcb8380a1c78368c649b9
Yasithb <150070850+Yasithb@users.noreply.github.com>
2025-03-04 00:38:21 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
 
 const Hero = () => {
   return (
     <div className='hero'>
-        
+        <img src={profile_img} alt="" />
       
     </div>
   )

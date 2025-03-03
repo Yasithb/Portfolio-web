49433b93ce99f873470dc5cfa33bf5d096b4667a
Yasithb <150070850+Yasithb@users.noreply.github.com>
2025-03-04 00:05:15 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         <li>About Me</li>
         <li>Services</li>
         <li>Portfolio</li>
-        <li></li>
+        <li>Contact</li>
         
       </ul>
     </div>

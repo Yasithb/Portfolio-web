5eea773dbaf630948cda0484b726eb6ea8ae28cc
Yasithb <150070850+Yasithb@users.noreply.github.com>
2025-03-05 11:15:35 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div className='hero'>
         <img src={profile_img} alt="" />
-        <h1>I'm Yasith Banula, Software Engineer based Sri Lanka</h1>
+        <h1><span>I'm Yasith Banula,</span> Software Engineer based Sri Lanka</h1>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, similique rem nisi at laborum nulla praesentium. Nihil animi corporis consequuntur optio enim repudiandae, obcaecati totam officia beatae omnis quae autem!</p>
         <div className="hero-action">
           <div className="hero-connect">Connect with me</div>

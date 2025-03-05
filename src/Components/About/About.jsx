5d466900f2d92f51702c09bad4959d172a93b52b
Yasithb <150070850+Yasithb@users.noreply.github.com>
2025-03-05 12:13:28 +0530

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -23,6 +23,7 @@ const About = () => {
             <div className="about-skills">
                 <div className="about-skill"><p>HTML & CSS</p>< hr style={{width:"50%"}} /></div>
                 <div className="about-skill"><p>HTML & CSS</p>< hr style={{width:"50%"}} /></div>
+                <div className="about-skill"><p>HTML & CSS</p>< hr style={{width:"50%"}} /></div>
                 
             </div>
         </div>

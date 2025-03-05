2ed72a3c1b724c2badb1fa7a182b545fea0dcaa6
Yasithb <150070850+Yasithb@users.noreply.github.com>
2025-03-05 11:02:39 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,6 +9,7 @@ const Hero = () => {
         <h1>I'm Yasith Banula, Software Engineer based Sri Lanka</h1>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, similique rem nisi at laborum nulla praesentium. Nihil animi corporis consequuntur optio enim repudiandae, obcaecati totam officia beatae omnis quae autem!</p>
         <div className="hero-action">
+          <div className="hero-connect">Connect with me</div>
           
         </div>
 

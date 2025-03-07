7ad9f8b7113ee8569f4398788457f6a2673b03e7
Yasithb <150070850+Yasithb@users.noreply.github.com>
2025-03-07 08:58:25 +0530

    Update Services.jsx

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Services.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
-
+import Services_Data from '../../assets/services_data'
 const Services = () => {
   return (
     <div className='services'>

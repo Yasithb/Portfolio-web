54747eff5b8d2e0aa7d2cfd6f03f17742a439e2e
Yasithb <150070850+Yasithb@users.noreply.github.com>
2025-03-08 09:28:18 +0530

    Update MyWork.jsx

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './MyWork.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
 
+
 const MyWork = () => {
   return (
     <div className='mywork'>
       <div className="mywork-title">
         <h1>My latest work</h1>
         <img src={theme_pattern} alt="" />
+      </div>
+      <div className="mywork-container">
+
       </div>
         
       
